d1da594a393fe4ecafc0cdaafe97e5c1c9abec1d
Walter Simon <simonwalter986@gmail.com>
2022-06-21 14:50:22 +0300

    commit tree 7fe612b9677edfe1781aea03a2d46af160481bbc

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased(tutorials) {
+   const  tutorial = [tutorials.titleCase()];
+    return tutorial;
 }
